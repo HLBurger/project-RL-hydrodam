d4677a7bcc9ec0de15c4add3649068eb191c53ca
HLBurger <hendricob.424@gmail.com>
2026-01-12 19:03:17 +0000

    commentje

--- policies.py
+++ policies.py
@@ -1,3 +1,4 @@
+import random
 def hourly_policy(observation):
     # The observation is the tuple: [volume, price, hour_of_day, day_of_week, day_of_year, month_of_year, year]
     if (observation[2] > 8) and (observation[2] < 23):  # If hour is between 9 AM and 10 PM, sell
@@ -17,6 +18,15 @@ def weekday_policy(observation):
 
     return action
 
+def month_policy(observation):
+    # The observation is the tuple: [volume, price, hour_of_day, day_of_week, day_of_year, month_of_year, year]
+    if (observation[5] >= 5) and (observation[5] <= 10):  # If month is May to October, sell
+        action = -1 # Sell
+    else:
+        action = 1 # Pump
+
+    return action
+
 def time_policy(observation):
     # The observation is the tuple: [volume, price, hour_of_day, day_of_week, day_of_year, month_of_year, year]
     # We combine both policies: we only sell when both policies agree to sell and only pump when both policies agree to pump, otherwise we do nothing.
@@ -30,3 +40,12 @@ def time_policy(observation):
         action = 0 # No action
 
     return action
+
+def epsilon_greedy_policy(observation, epsilon = 0.1):
+    p = random.random()
+    if p < epsilon:
+        action = random.choice([-1, 0, 1]) # Random action: Sell, No action, or Pump
+    else:
+        action = time_policy(observation) # Follow the time-based policy
+    return action
+
