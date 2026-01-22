9d8023a6d175131f26b8712a495b99ba5c32afc6
HLBurger <hendricob.424@gmail.com>
2026-01-22 18:30:35 +0100

    lr Decay toegevoegd

--- Qlearning.py
+++ Qlearning.py
@@ -54,9 +54,10 @@ class QAgent():
 
     def train(self, simulations, learning_rate,
               epsilon=0.1, epsilon_decay=1000,
-              adaptive_epsilon=False):
+              adaptive_epsilon=False, adaptive_learning_rate = True):
 
         self.create_Q_table()
+        self.starting_learning_rate = learning_rate
         self.learning_rate = learning_rate
 
         self.epsilon_start = 1
@@ -102,6 +103,9 @@ class QAgent():
                 total_rewards += reward
 
             self.learning_curve.append(total_rewards)
+            if adaptive_learning_rate:
+                self.learning_rate *= 0.99
+                
             if episode % 20 == 0:
                 print(episode,total_rewards)
 
