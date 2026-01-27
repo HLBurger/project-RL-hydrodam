f3b8684d3facd6a89a912b775bf9ccc2425a7b41
Joyesiam <joannepals@hotmail.com>
2026-01-27 14:23:47 +0100

    Verplaatsing + werkend krijgen

--- src/models/PPO.py
+++ src/models/PPO.py
@@ -1,18 +1,17 @@
+
 import gymnasium as gym
 import numpy as np
 from gymnasium.spaces import Discrete, Box
-from reward_shaping import reward_shaping
+from src.env.reward_shaping import reward_shaping
 
-class DQNWrapper(gym.Env):
-    def __init__(self, env, reward_shape, num_actions=3):
+
+class PPOWrapper(gym.Env):
+    def __init__(self, env, reward_shape = True):
         super().__init__()
         self.env = env
         self.max_episode_length = len(self.env.price_values.flatten())
-        self.actions = np.linspace(-1.0, 1.0, num_actions)
-        self.action_space = Discrete(num_actions)
+        self.action_space = Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
         self.reward_shape = reward_shape
-
-        # 5 discrete features
         self.observation_space = Box(low=0.0, high=1.0, shape=(5,), dtype=np.float32)
         self.action_history = []
 
@@ -33,14 +32,13 @@ class DQNWrapper(gym.Env):
         self.env.day = 1
         self.env.volume = self.env.max_volume / 2
         self.action_history = []
-        self.env.reset()
         obs = np.array(self.env.observation(), dtype=np.float32)
         return self.normalize(obs), {}
     
 
     def step(self, action):
         
-        real_action = self.actions[int(action)]
+        real_action = float(action[0])
         self.action_history.append(real_action)
 
         next_obs, reward, terminated, truncated, info = self.env.step(real_action)
@@ -48,10 +46,6 @@ class DQNWrapper(gym.Env):
 
         if self.reward_shape: 
             shaped_reward = reward_shaping(self.env, reward, self.action_history)
-
-        # Episode van 7 dagen (168 stappen)
-        # if self.env.counter % (1068) == 0 and self.env.counter > 0:
-        #     truncated = True
 
         next_obs = self.normalize(np.array(next_obs, dtype=np.float32))
 
